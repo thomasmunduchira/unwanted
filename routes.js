637dbcbd6663f387c7cbc1b2467331a62f468f31
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-10-07 16:52:43 -0700

    Analyze tweets

--- routes.js
+++ routes.js
@@ -11,51 +11,24 @@ router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-const processesCallback = (processesFinished, req) => {
-  if (processesFinished === 2) {
-    const instagramFolder = `./data/${req.session.id}/instagramData`;
-    return (
-      fs
-        .readdir(instagramFolder)
-        .then(filenames =>
-          Promise.all(
-            filenames.map(filename => fs.readFile(`${instagramFolder}/${filename}`, 'utf-8'))
-          )
-        )
-        .then(contents => {
-          const urls = [];
-          for (let i = 0; i < contents.length; i += 1) {
-            let content = contents[i];
-            content = JSON.parse(content);
-            urls.push(content.display_src);
-          }
-          console.log(urls);
-          return urls;
-        })
-        // .then(urls => analysis.visionRequest(urls))
-        .then(response => console.log(response))
-        .catch(err => {
-          console.log(err);
-        })
-    );
-  }
-};
-
-router.post('/data', (req, res) => {
+router.post('/data', req => {
   const { username } = req.body;
   const { id } = req.session;
-
-  let processesFinished = 0;
 
   console.log('twitterChild process initiated');
   exec(`python lib/twitter.py ${id} ${username}`)
     .then((stdout, stderr) => {
-      processesFinished += 1;
-      processesCallback(processesFinished, req, res);
       console.log('twitterChild process exited');
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
     })
+    .then(() => fs.readFile(`./data/${req.session.id}/twitterData.json`, 'utf-8'))
+    .then(content => {
+      content = JSON.parse(content);
+      return content;
+    })
+    .then(content => analysis.twitterFlagFinder(content.tweets))
+    .then(response => console.log(response))
     .catch(err => {
       console.log(err);
     });
@@ -63,12 +36,30 @@ router.post('/data', (req, res) => {
   console.log('instagramChild process initiated');
   exec(`python lib/instagram.py ${id} ${username}`)
     .then((stdout, stderr) => {
-      processesFinished += 1;
-      processesCallback(processesFinished, req, res);
       console.log('instagramChild process exited');
       console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
     })
+    .then(() => fs.readdir(`./data/${req.session.id}/instagramData`))
+    .then(filenames =>
+      Promise.all(
+        filenames.map(filename =>
+          fs.readFile(`./data/${req.session.id}/instagramData/${filename}`, 'utf-8')
+        )
+      )
+    )
+    .then(contents => {
+      const urls = [];
+      for (let i = 0; i < contents.length; i += 1) {
+        let content = contents[i];
+        content = JSON.parse(content);
+        urls.push(content.display_src);
+      }
+      console.log(urls);
+      return urls;
+    })
+    .then(urls => analysis.visionRequest(urls))
+    .then(response => console.log(response))
     .catch(err => {
       console.log(err);
     });
