3db4df7b7c2c0ad400c397d1c1267cd8f7840075
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-10-07 09:58:31 -0700

    Start up server and have it connect to the database

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const config = require('./config');
+const routes = require('./routes');
+
+const app = express();
+
+const env = process.env.NODE_ENV || 'development';
+
+mongoose.Promise = global.Promise;
+mongoose
+  .createConnection(config.db.url, {
+    useMongoClient: true,
+  })
+  .then(db => {
+    console.log('Connected to database!');
+
+    const sess = {
+      store: new MongoStore({
+        mongooseConnection: db,
+      }),
+      secret: config.session.secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {},
+    };
+    if (env === 'production') {
+      app.set('trust proxy', 1);
+      sess.cookie.secure = true;
+    }
+    app.use(session(sess));
+  })
+  .catch(() => {
+    console.error.bind(console, 'Connection error:');
+  });
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/', routes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  return next(err);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  if (err.status === 404) {
+    return res.render('error');
+  }
+  return res.json({
+    success: false,
+    message: 'Something unexpected happened. Please wait a bit and try again.',
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Unwanted site listening on port 8080!');
+});
---
+++ config_example.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    url: 'DB_URL',
+  },
+  session: {
+    secret: 'SESSION_SECRET',
+  },
+};
+
+module.exports = config;
